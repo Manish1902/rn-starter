4c2d15728a1895c4f56e49a3ffcd25f7ab6cd5c5
Manish Singh Talan <manishtalan19@gmail.com>
2024-04-30 09:22:41 +0530

    UI

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,21 +1,54 @@
 import React from 'react';
-import {View,StyleSheet, Text} from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 
 const BoxScreen = () => {
     return <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Hello there</Text>
+        {/* <Text style={styles.textOneStyle}>Child 1</Text>
+        <Text style={styles.textTwoStyle}>Child 2</Text>
+        <Text style={styles.textThreeStyle}>Child 3</Text> */}
+        <View style={styles.viewOneStyle} />
+        <View style={styles.viewTwoStyle} />
+        <View style={styles.viewThreeStyle} />
     </View>
 }
 
 const styles = StyleSheet.create({
     viewStyle: {
-        borderWidth: 1,
-        borderColor: black
+        borderWidth: 3,
+        borderColor: 'black',
+        height: 200,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
-    textStyle: {
-        borderWidth: 1,
-        backgroundColor: red
+    viewOneStyle: {
+        height: 50,
+        width: 50,
+        backgroundColor: 'red'
+    },
+    viewTwoStyle: {
+        height: 50,
+        width: 50,
+        backgroundColor: 'green',
+        top: 50
+    },
+    viewThreeStyle: {
+        height: 50,
+        width: 50,
+        backgroundColor: 'blue'
     }
+
+    // textStyle: {
+    //     borderWidth: 3,
+    //     backgroundColor: 'red',
+    // },
+    // textStyle: {
+    //     borderWidth: 3,
+    //     backgroundColor: 'green',
+    // },
+    // textStyle: {
+    //     borderWidth: 3,
+    //     backgroundColor: 'blue',
+    // }
 })
 
 export default BoxScreen
